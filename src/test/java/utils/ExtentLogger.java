22df5602e49fe216430d344b88efb407703516cc
stl2448 <thiru224488@gmail.com>
2026-03-19 17:02:37 +0530

    added step level logging

---
+++ src/test/java/utils/ExtentLogger.java
@@ -0,0 +1,27 @@
+package utils;
+
+import com.aventstack.extentreports.Status;
+import stepdefinitions.Hooks;
+
+public class ExtentLogger {
+
+    // ✅ Get current method name automatically
+    private static String getMethodName() {
+        return Thread.currentThread()
+                .getStackTrace()[3]   // 3 = caller method
+                .getMethodName()
+                .replace("_", " ");  // make readable
+    }
+
+    public static void logStep() {
+        Hooks.tlTest.get().log(Status.INFO, getMethodName());
+    }
+
+    public static void logPass() {
+        Hooks.tlTest.get().log(Status.PASS, getMethodName() + " - PASSED");
+    }
+
+    public static void logFail(String message) {
+        Hooks.tlTest.get().log(Status.FAIL, getMethodName() + " - FAILED: " + message);
+    }
+}
